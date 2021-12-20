22c87186352102f3ca54683b0ac4433414443a52
Blixzzy <96251292+Blixu@users.noreply.github.com>
2021-12-20 14:05:29 -0600

    Update and rename addTokens.js to AddTokens.js

--- global/AddTokens.js
+++ global/AddTokens.js
@@ -1,7 +1,7 @@
 //  --> PLEASE READ THIS!!! <---
 //  
 //  Do not change the numbers in this file! Blooket has a hard limit on how many tokens
-//  you can add everyday. The limit is 500 and you cannot bypass it. Do not mess with
+//  you can add everyday. The limit is 10k and you cannot bypass it. Do not mess with
 //  with this file if you do not know what you are doing!!!
 //
 
@@ -14,11 +14,11 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (10000 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
     if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.')
+        alert('You can only add up to 10000 tokens daily.')
     }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
